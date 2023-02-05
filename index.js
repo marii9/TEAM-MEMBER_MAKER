8134b3215157ca899a0a24dab1ae66a04cdd54cd
marii9 <116024194+marii9@users.noreply.github.com>
2023-02-05 01:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,89 +12,72 @@ if (!fs.existsSync(distDirectory)) {
   fs.mkdirSync(distDirectory);
 }
 
-// const questions = [
-//     {
-//         type: 'input',
-//         name: 'Name',
-//         message: 'Enter the team manager’s name:'
-//     },
-//     {
-//         type: 'input',
-//         name: 'ID',
-//         message: 'Enter the team manager’s ID:'
-//     },
-//     {
-//         type: 'input',
-//         name: 'Email',
-//         message: 'Enter the team manager’s email address:'
-//     },
-//     {
-//         type: 'input',
-//         name: 'Office Number',
-//         message: 'Enter the team manager’s office number:'
-//     }
-// ];
+
 
 const team = [];
 
 const askForTeamMember = () => {
-    return inquirer.prompt([
-      {
-        type: 'list',
-        name: 'Role',
-        message: 'Select the role for the next team member:',
-        choices: ['Engineer', 'Intern', 'Manager', 'No more team members'],
-      },
-      {
-        type: 'input',
-        name: 'Name',
-        message: 'Enter the team member’s name:',
-      },
-      {
-        type: 'input',
-        name: 'ID',
-        message: 'Enter the team member’s ID:',
-      },
-      {
-        type: 'input',
-        name: 'Email',
-        message: 'Enter the team member’s email address:',
-      },
-      {
-        when: function (answers) {
-          return answers.Role === 'Engineer';
-        },
-        type: 'input',
-        name: 'GitHub',
-        message: 'Enter the engineer’s GitHub username:',
-      },
-      {
-        when: function (answers) {
-          return answers.Role === 'Intern';
-        },
-        type: 'input',
-        name: 'School',
-        message: 'Enter the intern’s school:',
-      },
-      {
-        when: function (answers) {
-          return answers.Role === 'Manager';
-        },
-        type: 'input',
-        name: 'Office Number',
-        message: 'Enter the manager’s office number:',
-      },
-    ]);
-  };
-
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'Role',
+                message: 'Select the role for the next team member:',
+                choices: ['Manager', 'Engineer', 'Intern', 'No more team members']
+            },
+            {  when: function(answers) {
+                return answers.Role!== 'No more team members';
+            },
+                type: 'input',
+                name: 'Name',
+                message: 'Enter the team member’s name:'
+            },
+            {
+                when: function(answers) {
+                    return answers.Role!== 'No more team members';
+                },
+                type: 'input',
+                name: 'ID',
+                message: 'Enter the team member’s ID:'
+            },
+            {
+                when: function(answers) {
+                    return answers.Role!== 'No more team members';
+                },
+                type: 'input',
+                name: 'Email',
+                message: 'Enter the team member’s email address:'
+            },
+            {
+                when: function(answers) {
+                    return answers.Role === 'Engineer';
+                },
+                type: 'input',
+                name: 'GitHub',
+                message: 'Enter the engineer’s GitHub username:'
+            },
+            {
+                when: function(answers) {
+                    return answers.Role === 'Intern';
+                },
+                type: 'input',
+                name: 'School',
+                message: 'Enter the intern’s school:'
+            },
+            {
+                when: function(answers) {
+                    return answers.Role === 'Manager';
+                },
+                type: 'input',
+                name: 'Office Number',
+                message: 'Enter the manager’s office number:'
+            }
+        ]);
+};
 const askForTeamMembers = () => {
-    inquirer
-        .prompt(askForTeamMember)
+    askForTeamMember()
         .then(answers => {
-            team.push(answers);
-            return askForTeamMember();
-        })
-        .then(answers => {
+            console.log(answers)
             if (answers.Role === 'No more team members') {
                 const filePath = path.join(distDirectory, 'team.html');
                 fs.writeFile(filePath, generateHTML(team), err => {
